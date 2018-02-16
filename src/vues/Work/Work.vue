5a488ded3da50b9b123b3bb4a1dfc59af4477c45
Valérian Polizzi <valerian.polizzi@epitech.eu>
2018-02-16 13:07:40 +0100

    added fb sharing pic

--- src/vues/Work/Work.vue
+++ src/vues/Work/Work.vue
@@ -15,6 +15,14 @@
 
 <script>
 
+(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
     import Projects from '../../assets/data/projects';
     import Valer from '../../assets/images/portfolio/valerian.png';
 
