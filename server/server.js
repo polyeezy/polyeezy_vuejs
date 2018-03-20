2f55bac076d06d7a6c481d81ec530634e248bd3a
Valérian Polizzi <valerian.polizzi@epitech.eu>
2018-03-20 18:24:42 +0100

    added docker

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var express = require('express');
+path = require('path'),
+    app = express();
 
-connect().use(serveStatic(__dirname + '/../app/dist')).listen(8080, function(){
+app.set('port', 8080);
 
-    console.log(__dirname + '../app/dist');
-    console.log('Server running on 8080...');
+//tell express that we want to use the www folder
+//for our static assets
+app.use(express.static(path.join(__dirname, '../app/dist')));
+console.log(__dirname, '../app/dist');
+// Listen for requests
+var server = app.listen(app.get('port'), function () {
+    console.log('The server is running on http://localhost:' + app.get('port'));
 });
